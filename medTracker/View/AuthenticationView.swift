7feba7c1a8a8bb644882948b7b1ab5b9c4b59ecf
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-12 16:08:06 -0600

    Move code from the main file to an second file to not clutter authentication and loggin in with the source of the project.

---
+++ medTracker/View/AuthenticationView.swift
@@ -0,0 +1,76 @@
+//
+//  Bienvenida.swift
+//  medTracker
+//
+//  Created by Alumno on 09/11/23.
+//
+
+import SwiftUI
+
+/**********************
+ This view only displays a welcome message and two buttons to log in or register.
+ **********************************/
+struct AuthenticationView: View {
+    @EnvironmentObject var authViewModel: AuthViewModel
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color("mainWhite")
+                    .ignoresSafeArea()
+                VStack{
+                    if colorScheme == .light{
+                        Image("logoV")
+                            .resizable()
+                            .imageScale(.small)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 300)
+                    }
+                    else{
+                        Image("logoDarkMode")
+                            .resizable()
+                            .imageScale(.small)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 300)
+                    }
+                    NavigationLink {
+                        LogInView(signInModel: authViewModel.makeSignInViewModel())
+                    } label: {
+                        Text("Iniciar Sesión")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 220, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color("mainBlue"), Color("blueGreen")]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    }
+                    
+                    NavigationLink {
+                        RegisterView(createAccountModel: authViewModel.makeCreateAccountViewModel())
+                    } label: {
+                        Text("Registrarse")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 220, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color("mainBlue"), Color("blueGreen")]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+struct Bienvenida_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
+    }
+}
--- medTracker/medTrackerApp.swift
+++ medTracker/medTrackerApp.swift
@@ -10,58 +10,14 @@ import Firebase
 
 @main
 struct medTrackerApp: App {
-    @AppStorage("hasRequestedNotificationPermission") private var hasRequestedNotificationPermission = false
     // Initialize the configuration of the database
     init() {
         FirebaseApp.configure()
     }
-    @StateObject var authViewModel = AuthViewModel()
-    
+
     var body: some Scene {
         WindowGroup {
-            if let userRole = authViewModel.user?.rol {
-                if userRole == "Paciente" {
-                    if let symptomList = authViewModel.makeSymptomList(),
-                       let registersList = authViewModel.makeRegisterList(),
-                       let userModel = authViewModel.makeUserModel() {
-                        MainView(
-                            symptoms: symptomList, registers: registersList, user: userModel
-                        )
-                        .environmentObject(authViewModel)
-                    } else {
-                        Text("Didn't work")
-                    }
-                } else if userRole == "Doctor" {
-                    if let userModel = authViewModel.makeUserModel(),
-                       let patientList = authViewModel.makePatientList() {
-                        MainDoctorView(user: userModel, patientsList: patientList)
-                        .environmentObject(authViewModel)
-                    } else {
-                        Text("Didn't work")
-                    }
-                } else {
-                   ProgressView()
-                }
-            } else {
-                WelcomeView()
-                    .environmentObject(authViewModel)
-                    .onAppear(perform: {
-                        if !hasRequestedNotificationPermission {
-                            requestNotificationPermission()
-                            hasRequestedNotificationPermission = true
-                        }    
-                    })
-            }
-        }
-    }
-}
-
-func requestNotificationPermission() {
-    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-        if success {
-            print("Permission granted!")
-        } else if let error = error {
-            print(error.localizedDescription)
+            WelcomeView()
         }
     }
 }
